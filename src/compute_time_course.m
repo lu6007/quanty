42bd330c6aa5c5599b7847c1ea509a85fc69e8d5
sjpl <slaub@ucsd.edu>
2016-08-22 13:11:32 -0700

    Fixed g2p_quantify() figure plotting for multiple objects.

--- src/compute_time_course.m
+++ src/compute_time_course.m
@@ -1,6 +1,6 @@
 % Compute the time course of FRET ratio for a single cell
 % 
-% Examaple:
+% Example:
 % See g2p_quantify() for details.
 
 % Copyright: Shaoying Lu and Yingxiao Wang 2013 
@@ -93,6 +93,9 @@ end;
 clear path output_path;
 
 
+%% Plotting the quantifications.
+
+%%% Configuring the subplots. %%%
 if subplot_position,
     enable_subplot = 1;
     if ~compute_cell_size,
@@ -101,31 +104,52 @@ if subplot_position,
         num_figures = 5;
     end;
     subm = 2; % number of rows
-    subn = 3; % number of colums
+    subn = 3; % number of columns
     fn = 2+floor((subplot_position-1)/(subm*subn))*num_figures;
     subplot_position = mod(subplot_position-1, subm*subn)+1;
 else
     enable_subplot = 0;
 end;
 
+
+
+%%% cfp and yfp Intensities over Index figure. %%%
 if ~enable_subplot,
     figure; 
 else
     figure(fn+1); subplot(subm,subn,subplot_position);
 end;
-
 % make the font and size more fit for publications
 set(gca,'FontSize', 12,'FontName','Arial', 'Fontweight', 'bold');
 set(findall(gcf,'type','text'),'FontSize', 12,'FontName','Arial', 'Fontweight', 'bold');
 hold on;
-plot(this_image_index, cfp_intensity{1, 1}(this_image_index,1), 'b','LineWidth',2); 
-plot(this_image_index, yfp_intensity{1, 1}(this_image_index,1), 'g','LineWidth',2);
+
+%Problem: cfp_intensity always seems to be AT LEAST two cells long,
+%irrespective of the actual number of objects in an image.
+%Current workaround: set num_objects based on the fret_ratio, which seems to
+%reliably reflect the actual number of objects in an image. -Shannon 8/10/2016
+% num_objects = length(cfp_intensity);
+num_objects = length(fret_ratio);
+for i = 1:num_objects
+    plot(this_image_index, cfp_intensity{i}(this_image_index,1), 'b','LineWidth',2); 
+    plot(this_image_index, yfp_intensity{i}(this_image_index,1), 'g','LineWidth',2);
+end
 plot(this_image_index, cfp_background(this_image_index), 'b--','LineWidth',2);
-plot(this_image_index, yfp_background(this_image_index), 'g--', 'LineWidth',2);
+plot(this_image_index, yfp_background(this_image_index), 'g--', 'LineWidth',2);  
+    
+% plot(this_image_index, cfp_intensity{1, 1}(this_image_index,1), 'b','LineWidth',2); 
+% plot(this_image_index, yfp_intensity{1, 1}(this_image_index,1), 'g','LineWidth',2);
+% plot(this_image_index, cfp_background(this_image_index), 'b--','LineWidth',2);
+% plot(this_image_index, yfp_background(this_image_index), 'g--', 'LineWidth',2);
+
+%Set title and legend for figure.
 title(regexprep(cell_name,'_','\\_'));
 xlabel('Index'); ylabel('Intensity');
-legend('1^{st}FI','2^{nd} FI','1^{st} FI BG', '2^{nd} FI BG');
-%
+legend('1^{st} FI','2^{nd} FI','1^{st} FI BG', '2^{nd} FI BG');
+
+
+
+%%% Index over time figure. %%%
 if ~enable_subplot,
     figure; 
 else
@@ -134,10 +158,14 @@ end;
 set(gca,'FontSize',12,'FontName','Arial', 'Fontweight', 'bold')
 set(findall(gcf,'type','text'),'FontSize',12,'FontName','Arial', 'Fontweight', 'bold');
 hold on;
-plot(time(data.image_index,2), 'LineWidth',2);
-% plot(time(:, 2), 'LineWidth',2);
-xlabel('Index'); ylabel('Time');
-%
+% plot(time(data.image_index,2), 'LineWidth',2);
+plot(time(:, 2), 'LineWidth',2);
+title(regexprep(cell_name,'_','\\_'));
+xlabel('Index'); ylabel('Time (min)');
+
+
+
+%%% Intensity Ratio over Index figure. %%%
 if ~enable_subplot,
     figure; 
 else
@@ -146,19 +174,38 @@ end;
 set(gca,'FontSize',12,'FontName','Arial', 'Fontweight', 'bold')
 set(findall(gcf,'type','text'),'FontSize',12,'FontName','Arial', 'Fontweight', 'bold')
 hold on;
+
+%Created for loop to manage plotting multiple objects. - Shannon 8/9/2016
+%Converts from fret_ratio's cell data type to a double data type and also
+%shrinks the matrix to match this_image_index's size so it will plot correctly.
+%Preallocation of matrix space for this_fret_ratio.
+num_image_indices = length(this_image_index);
 num_objects = length(fret_ratio);
-if num_objects ==1,
-    this_fret_ratio = fret_ratio{1}(this_image_index, 1);
-else % num_objects == 2
-    this_fret_ratio = [fret_ratio{1}(this_image_index, 1), fret_ratio{2}(this_image_index, 1)];
-end;
+this_fret_ratio = inf(num_image_indices,num_objects);
+%Assigns the corresponding fret_ratio value to the matrix.
+for i = 1:num_objects
+    this_fret_ratio(:,i) = fret_ratio{i}(this_image_index,1);
+end
+
+% Assumes only 1 or 2 objects for now.
+% Plan to expand to have a loop for n-objects.
+% num_objects = length(fret_ratio);
+% if num_objects == 1,
+%     this_fret_ratio = fret_ratio{1}(this_image_index, 1);
+% else % num_objects == 2
+%     this_fret_ratio = [fret_ratio{1}(this_image_index, 1), fret_ratio{2}(this_image_index, 1)];
+% end;
+
 plot(this_image_index, this_fret_ratio, 'r','LineWidth',2);
 %plot(this_image_index, fret_ratio{1}(this_image_index, 1), 'r','LineWidth',2);
 title(regexprep(cell_name,'_','\\_'));
 % xlabel('Index'); ylabel('ECFP/FRET Ratio');
 % change the name of the figure 
 xlabel('Index'); ylabel('Intensity Ratio');
-%
+
+
+
+%%% Intensity Ratio over Time (min) figure. %%%
 if ~enable_subplot,
     figure; 
 else
@@ -171,7 +218,9 @@ plot(time(this_image_index, 2), this_fret_ratio, 'r','LineWidth',2);
 title(regexprep(cell_name,'_','\\_'));
 xlabel('Time (min)'); ylabel('Intensity Ratio');
 clear this_fret_ratio;
-%
+
+
+
 %%%%%%%%10/21/2014 lexie plot cell size change
 if compute_cell_size,
     if ~enable_subplot,
@@ -189,11 +238,9 @@ if compute_cell_size,
     xlabel('Index'); ylabel('Cell Size');
 end;
 
-% output
+%% output
 temp = time(this_image_index); clear time;
 time = temp;
 value = fret_ratio{1}(this_image_index); 
 beep;
 return;
-
-
