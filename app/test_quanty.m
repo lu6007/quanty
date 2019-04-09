fd9427cf880af64e3d951883242fe74aadb54724
lu6007 <shaoying.lu@gmail.com>
2019-04-09 16:37:10 -0700

    Added a test funvtion for quanty.

---
+++ app/test_quanty.m
@@ -0,0 +1,23 @@
+% test_quanty
+
+%%For each part, please right click and choose "Evaluate current section"
+%%or use ctrl+enter.
+if ~exist('enable_pause', 'var')
+    enable_pause = 1;
+end
+fprintf('test_flucell: enable_pause = %d\n', enable_pause);
+
+%% 
+test_fig2
+
+%%
+test_fig3
+
+%%
+test_fig4
+
+%%
+test_fig5
+
+%%
+% test_group_plot
